459632071d086bc20602bc3e15c9dc24508a64cc
Diego Garcia <d.garcia07@outlook.com>
2021-03-05 19:40:16 -0800

    basic routing with html files

--- requests-responses/server.js
+++ requests-responses/server.js
@@ -1,25 +1,39 @@
-const http = require('http');
-const hostname = 'localhost';
+const http = require("http");
+const hostname = "localhost";
 const port = 8000;
-const fs = require('fs');
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    // send an html file
-    fs.readFile('./views/index.html', (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end();
-        } else {
-            //res.write(data);
-            res.end(data);
-        }
-    })
-    
+  console.log(req.url, req.method);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  // Send an html file
+  // We can also figure out the path a user can take
+  // simple routing system 
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //res.write(data);
+      res.end(data);
+    }
+  });
 });
 
-server.listen(port, hostname,  () => {
-    console.log('Listening on port 3000'); 
-})
+server.listen(port, hostname, () => {
+  console.log("Listening on port 3000");
+});
